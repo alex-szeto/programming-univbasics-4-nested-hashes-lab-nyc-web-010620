c5617c22659e9724c97fdb5774ae77e7174f359b
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 00:08:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
         }
      }
 
-    #programmer_hash[:yukihiro_matsumoto] = {}
+    programmer_hash[:yukihiro_matsumoto] = {}
     programmer_hash[:yukihiro_matsumoto][:known_for] => "Ruby"
     programmer_hash[:yukihiro_matsumoto][:languages] => ["LISP", "C"]
     return programmer_hash
@@ -106,7 +106,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     programmer_hash[:alan_kay][]
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
 end
 
 def adding_to_dennis
